584d6260fb0dc172881d011ea66d8e1fe86af012
Sean Michael Suntoso <ssuntoso@gmail.com>
2022-08-06 08:55:31 +0800

    bug fix and update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Find and verify work emails.'
 
 # Setting up
@@ -19,11 +19,12 @@ setup(
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/ssuntoso/emailfinderpy',
     packages=find_packages(),
     install_requires=['numpy', 'pandas', 'dnspython'],
     keywords=['python', 'email', 'email finder', 'work email', 'company domain', 'start up'],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
